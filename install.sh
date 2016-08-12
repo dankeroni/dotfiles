2e65c9c755447b8c42df604aaeb898ee50b5208e
dankeroni <pana99@hotmail.de>
2016-08-12 23:37:54 +0300

    Added a xsessionrc
    This disables the gnome password pop-ups

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing dankeroni's dotfiles..."
 
 directories=(.config/nvim)
 files=(.bash_aliases .bash_exports .bashrc .gitconfig .gitignore .profile .config/nvim/init.vim \
-    .tmux.conf)
+    .tmux.conf .xsessionrc)
 
 for directory in "${directories[@]}"; do
     mkdir -p ~/$directory
