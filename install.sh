bee2646a254a5280b6d7f668655fe56a96a79629
dankeroni <theofpanagiotis@gmail.com>
2021-10-06 22:59:29 +0200

    Added gpg-agent conf

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 echo "Installing dankeroni's dotfiles..."
 
-directories=(.config/nvim .config/awesome .icons/default)
+directories=(.config/nvim .config/awesome .icons/default .gnupg)
 files=(.bash_aliases .bash_exports .bashrc .gitconfig .gitignore .profile .config/nvim/init.vim \
     .tmux.conf .config/awesome/rc.lua .icons/default/index.theme .gdbinit .vimrc commitlint.config.js \
-    .editorconfig .clang-format)
+    .editorconfig .clang-format .gnupg/gpg-agent.conf)
 
 for directory in "${directories[@]}"; do
     mkdir -p ~/$directory
